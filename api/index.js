2ed85c7d5cb6b2c0b853d47d16a5465f94e30e39
Potiekhin Oleh <potiehin.o@gmail.com>
2023-11-27 14:07:42 +0200

    created middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -16,9 +16,17 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server startted on port ${PORT}`));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
