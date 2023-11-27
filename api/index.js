e6c66b6e1e1d582713b8528fe89353acbe238bde
Potiekhin Oleh <potiehin.o@gmail.com>
2023-11-27 13:33:42 +0200

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -13,6 +15,10 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server startted on port ${PORT}`));
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
